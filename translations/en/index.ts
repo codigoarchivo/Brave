e1e79a8eef565d8485354349da646b79fd793678
jackson jose quintero angulo <codigoarchivo@gmail.com>
2022-08-19 09:40:36 -0400

    se creo carpeta translation, completada pageHome translations, pageLayout en proceso

---
+++ translations/en/index.ts
@@ -0,0 +1,14 @@
+export const en = {
+   pageLayout: {
+      a: 'Home',
+      b: 'About Me',
+      c: 'Services',
+      d: 'work',
+      e: 'Contact',
+   },
+   pageHome: {
+      a: 'Port',
+      b: 'Folio',
+      c: 'FullStack Developer',
+   },
+};
---
+++ translations/es/index.ts
@@ -0,0 +1,14 @@
+export const es = {
+  pageLayout: {
+    a: 'Hogar',
+    b: 'Sobre Mí',
+    c: 'Servicios',
+    d: 'Trabajó',
+    e: 'Contacto',
+  },
+  pageHome: {
+    a: 'Porta',
+    b: 'Folio',
+    c: 'Desarrollador De Pila Completa',
+  },
+};
