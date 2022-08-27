e9c59336966794dd30fb41cce772c4e17bb8c45f
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-08-27 12:07:47 -0400

    cambie la carpeta services a skills

---
+++ pages/skills/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+
+import { Layout } from '../../components/layouts'
+
+const SkillsPage: NextPage = () => {
+    return (
+        <Layout title={"Services"}>
+            <div>Services</div>
+        </Layout>
+    )
+}
+
+export default SkillsPage
