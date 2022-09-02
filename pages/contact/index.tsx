d43c29372d82f0909cb363b0c39e9e943a026873
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-09-02 18:47:37 -0400

    constact/ se agregaron  pequeños  cambios

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -6,19 +6,18 @@ import Grid from '@mui/material/Grid';
 
 import { Layout } from '../../components/layouts'
 
-import { ContactScreen } from '../../components/ui/contact/ContactScreen';
+import { InformationScreen, ContactScreen } from '../../components/ui/contact';
 
 const ContactPage: NextPage = () => {
   return (
     <Layout title={"Contact"}>
-      <Container maxWidth="lg" sx={{ margin: { xs: '80px 0', sm: '100px 0', md: '150px 0' } }}>
-
-        <Grid container spacing={{ xs: 2, sm: 5, md: 0 }} sx={{ height: 'calc(100vh - 250px)', backgroundColor: 'background.default' }}>
+      <Container maxWidth='xl' sx={{ margin: { xs: '80px 0', sm: '100px 0', md: '160px 0' } }}>
+        <Grid container spacing={6} sx={{ height: 'calc(100vh - 250px)', backgroundColor: 'background.default' }}>
           <Grid item xs={12} sm={12} md={6} display={'flex'} justifyContent={'center'} alignItems={'center'} flexDirection={'column'} >
-
+            <InformationScreen />
           </Grid>
-          <Grid item xs={12} sm={12} md={6} display={'flex'} justifyContent={'center'} alignItems={"center"} sx={{ position: 'relative' }}>
-             <ContactScreen />
+          <Grid item xs={12} sm={12} md={6} display={'flex'} justifyContent={'center'} alignItems={"center"} sx={{ paddingBottom: { xs: 3, lg: 0 } }}>
+            <ContactScreen />
           </Grid>
         </Grid>
       </Container>
