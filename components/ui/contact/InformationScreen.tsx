d4a410ed317e59bdaf022e0a199c68ac397f50bc
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-09-03 09:14:27 -0400

    InformationScreen: agrege texto en es

--- components/ui/contact/InformationScreen.tsx
+++ components/ui/contact/InformationScreen.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router';
+
 import CallIcon from '@mui/icons-material/Call';
 
 import Divider from '@mui/material/Divider';
@@ -14,28 +16,31 @@ import Typography from '@mui/material/Typography';
 
 import { styled } from '@mui/material/styles';
 
+import { en, es } from '../../../translations';
+
 export const InformationScreen = () => {
 
+    const { locale } = useRouter()
+
     const Item = styled(Paper)(({ theme }) => ({
-        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        backgroundColor: theme.palette.mode === 'dark' ? '#121212' : '#fff',
         ...theme.typography.body2,
         padding: theme.spacing(1),
         textAlign: 'center',
         color: theme.palette.text.secondary,
     }));
 
-
     return (
         <Stack spacing={{ xs: 1, sm: 2, md: 4 }} sx={{ width: '100%' }}>
             <Item>
                 <Typography variant={'h3'} fontWeight={600} textTransform={'uppercase'}>
-                    Contact
+                    {locale === 'en' ? en.pageContact.a : es.pageContact.a}
                 </Typography>
                 <Stack sx={{ marginBottom: 3 }} justifyContent={'center'} direction="row">
                     <Typography variant={'h3'} fontWeight={600} textTransform={'uppercase'}>
-                        With
+                        {locale === 'en' ? en.pageContact.b : es.pageContact.b}
                     </Typography>
-                    <Typography sx={{ marginLeft: '25px' }} variant={'h3'} color='primary.main' fontWeight={600} textTransform={'uppercase'}>Me</Typography>
+                    <Typography sx={{ marginLeft: locale === 'en' ? '25px' : 0 }} variant={'h3'} color='primary.main' fontWeight={600} textTransform={'uppercase'}>{locale === 'en' ? en.pageContact.c : es.pageContact.c}</Typography>
                 </Stack>
             </Item>
             <div>
@@ -44,7 +49,7 @@ export const InformationScreen = () => {
                     "&::before, &::after": {
                         borderColor: "secondary.light",
                     }
-                }}>More Information</Divider>
+                }}>{locale === 'en' ? en.pageContact.d : es.pageContact.d}</Divider>
             </div>
             <Item>
                 <Stack sx={{ marginBottom: 3 }} justifyContent={'space-between'} direction="row"><EmailIcon color='primary' /> <Typography variant='body1'>codigoarchivo@gmail.com</Typography></Stack>
