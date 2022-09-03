637b3f0eace15a07fff2279556cbd7b12408e0a7
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-09-03 09:13:03 -0400

    ContactScreen: agrege texto en es

--- components/ui/contact/ContactScreen.tsx
+++ components/ui/contact/ContactScreen.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router'
+
 import Button from '@mui/material/Button'
 
 import Card from '@mui/material/Card'
@@ -10,37 +12,38 @@ import TextField from '@mui/material/TextField'
 
 import Typography from '@mui/material/Typography'
 
+import { en, es } from '../../../translations'
+
 export const ContactScreen = () => {
+    const { locale } = useRouter()
+
     return (
         <>
             <Grid>
                 <Card sx={{ padding: "20px 5px", margin: "0 auto" }}>
                     <CardContent>
-                        <Typography gutterBottom variant="h5">
-                            Contact Us
-                        </Typography>
                         <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
-                            Fill up the form and our team will get back to you within 24 hours.
+                            {locale === 'en' ? en.pageContact.e : es.pageContact.e}
                         </Typography>
                         <form>
                             <Grid container spacing={1}>
                                 <Grid xs={12} sm={6} item>
-                                    <TextField placeholder="Enter first name" label="First Name" variant="outlined" fullWidth required />
+                                    <TextField placeholder={locale === 'en' ? en.pageContact.g : es.pageContact.g} label={locale === 'en' ? en.pageContact.f : es.pageContact.f} variant="outlined" fullWidth required />
                                 </Grid>
                                 <Grid xs={12} sm={6} item>
-                                    <TextField placeholder="Enter last name" label="Last Name" variant="outlined" fullWidth required />
+                                    <TextField placeholder={locale === 'en' ? en.pageContact.i : es.pageContact.i} label={locale === 'en' ? en.pageContact.h : es.pageContact.h} variant="outlined" fullWidth required />
                                 </Grid>
                                 <Grid item xs={12}>
-                                    <TextField type="email" placeholder="Enter email" label="Email" variant="outlined" fullWidth required />
+                                    <TextField type="email" placeholder={locale === 'en' ? en.pageContact.k : es.pageContact.k} label={locale === 'en' ? en.pageContact.j : es.pageContact.j} variant="outlined" fullWidth required />
                                 </Grid>
                                 <Grid item xs={12}>
-                                    <TextField type="number" placeholder="Enter phone number" label="Phone" variant="outlined" fullWidth required />
+                                    <TextField type="number" placeholder={locale === 'en' ? en.pageContact.ll : es.pageContact.ll} label={locale === 'en' ? en.pageContact.l : es.pageContact.l} variant="outlined" fullWidth required />
                                 </Grid>
                                 <Grid item xs={12}>
-                                    <TextField label="Message" multiline rows={4} placeholder="Type your message here" variant="outlined" fullWidth required />
+                                    <TextField label={locale === 'en' ? en.pageContact.m : es.pageContact.m} multiline rows={4} placeholder={locale === 'en' ? en.pageContact.n : es.pageContact.n} variant="outlined" fullWidth required />
                                 </Grid>
                                 <Grid item xs={12}>
-                                    <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
+                                    <Button type="submit" variant="contained" color="primary" fullWidth>{locale === 'en' ? en.pageContact.o : es.pageContact.o}</Button>
                                 </Grid>
 
                             </Grid>
