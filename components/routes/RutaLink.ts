e06156fecce2052c6a6359bf96abb94bc963f68b
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-10-31 21:32:47 -0400

    components/routes/: cree una carpeta routes coloque rutas

---
+++ components/routes/RutaLink.ts
@@ -0,0 +1,52 @@
+import { useRouter } from 'next/router';
+
+import ContactPageIcon from '@mui/icons-material/ContactPage';
+
+import DesignServicesIcon from '@mui/icons-material/DesignServices';
+
+import HomeIcon from '@mui/icons-material/Home';
+
+import PersonIcon from '@mui/icons-material/Person';
+
+import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
+
+import { en, es } from '../../translations';
+
+
+
+export const RutaLink = () => {
+
+  const { locale } = useRouter()
+
+  const data = [
+    {
+      href: "/",
+      name: locale === "en" ? en.pageLayout.a : es.pageLayout.a,
+      startIcon: HomeIcon
+    },
+    {
+      href: "/about",
+      name: locale === "en" ? en.pageLayout.b : es.pageLayout.b,
+      startIcon: PersonIcon
+    },
+    {
+      href: "/skills",
+      name: locale === "en" ? en.pageLayout.c : es.pageLayout.c,
+      startIcon: DesignServicesIcon
+    },
+    {
+      href: "/work",
+      name: locale === "en" ? en.pageLayout.d : es.pageLayout.d,
+      startIcon: WorkHistoryIcon
+    },
+    {
+      href: "/contact",
+      name: locale === "en" ? en.pageLayout.e : es.pageLayout.e,
+      startIcon: ContactPageIcon
+    }
+  ]
+
+  return data
+}
+
+
