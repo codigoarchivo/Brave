0956ecbd111ed3718f8ebf09672e855b3585b954
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-11-02 15:35:00 -0400

    pages/contact/: quite un desbordamiento, quite un container

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -11,16 +11,37 @@ import { InformationScreen, ContactScreen } from '../../components/contact';
 const ContactPage: NextPage = () => {
   return (
     <Layout title={"Contact"}>
-      <Container maxWidth='xl' sx={{ margin: { xs: '80px 0', sm: '100px 0', md: '160px 0' } }}>
-        <Grid container spacing={6} sx={{ height: 'calc(100vh - 250px)', backgroundColor: 'background.default' }}>
-          <Grid item xs={12} sm={12} md={6} display={'flex'} justifyContent={'center'} alignItems={'center'} flexDirection={'column'} >
-            <InformationScreen />
-          </Grid>
-          <Grid item xs={12} sm={12} md={6} display={'flex'} justifyContent={'center'} alignItems={"center"} sx={{ paddingBottom: { xs: 3, lg: 0 } }}>
-            <ContactScreen />
-          </Grid>
+      <Grid
+        display={'flex'}
+        alignItems={'center'}
+        container
+        spacing={6}
+        sx={{ height: '100vh', backgroundColor: 'background.default' }}
+      >
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={6}
+          display={'flex'}
+          justifyContent={'center'}
+          alignItems={'center'}
+          flexDirection={'column'} >
+          <InformationScreen />
         </Grid>
-      </Container>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={6}
+          display={'flex'}
+          justifyContent={'center'}
+          alignItems={"center"}
+          sx={{ paddingBottom: { xs: 3, lg: 0 } }}
+        >
+          <ContactScreen />
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
