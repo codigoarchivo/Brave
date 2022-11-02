885fc50b1e1c51f7174e70eb127513c60661ea96
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-11-01 21:44:39 -0400

    components/ui/Breadcrumb.tsx: agregue palatte secondary

--- components/ui/Breadcrumb.tsx
+++ components/ui/Breadcrumb.tsx
@@ -6,15 +6,19 @@ import {  SomeButton } from './';
 
 import { RutaLink } from '../routes';
 
+import { useTheme } from '@mui/material';
+
 export const Breadcrumb = () => {
   // rutaLink
-  const data = RutaLink()
+  const data = RutaLink();
 
+  const theme = useTheme();
+  
   return (
     <Breadcrumbs aria-label="breadcrumb" sx={{ padding: "5px 35px" }}>
       {
         data.map((item, key) =>
-          <SomeButton {...item} color={'secondary'} startIcon={<item.startIcon />} key={key} size={"small"} />
+          <SomeButton {...item} color={theme.palette.text.secondary} variant={'primary'} startIcon={<item.startIcon />} key={key} size={"small"} />
         )
       }
     </Breadcrumbs>
