addf8054682aea88e6fa17dd4865a4225c88ce52
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-11-01 21:51:34 -0400

    utils/color_palette.ts agregue nuevas constantes color

--- utils/color_palette.ts
+++ utils/color_palette.ts
@@ -1,1 +1,5 @@
-export const COLOR_ONE = '#2f314e';
+export const COLOR_ONE = '#2f92b7';
+export const COLOR_TWO = '#fff';
+export const COLOR_THREE = '#69c2e9';
+export const COLOR_FOUR = '#69c2e9 2px 2px 20px 0px';
+export const COLOR_FIVE = '#00000099';
