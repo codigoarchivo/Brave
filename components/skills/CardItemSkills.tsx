06ddae24f668bfb902ab79933fb3fa00d9d6c27d
Jackson Quintero <codigoarchivo@gmail.com>
2022-12-03 17:32:38 -0400

    components/skills/CardItemSkills.tsx: modificaciones menores en el card con respecto al tamaño

--- components/skills/CardItemSkills.tsx
+++ components/skills/CardItemSkills.tsx
@@ -1,60 +1,53 @@
-import React, { FC } from 'react'
-
+import { FC } from 'react'
 import Image from 'next/image';
-
 import Box from '@mui/material/Box';
-
 import Card from '@mui/material/Card';
-
 import CardContent from '@mui/material/CardContent';
-
 import Divider from '@mui/material/Divider'
-
 import Grid from '@mui/material/Grid'
-
-import Stack from '@mui/material/Stack'
-
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-
 import { capitalize } from '@mui/material';
+import { IEntry } from '../../interfaces';
 
-interface ProspData {
-    image: string;
-    name: string;
-}
 
-export const CardItemSkills: FC<ProspData> = ({ image, name }) => {
+export const CardItemSkills: FC<IEntry> = ({ image, name }) => {
     return (
         <Grid
             item
             xs={12}
-            sm={3}
-            md={3}
-            lg={2}
-            xl={1}
+            sm={6}
+            md={4}
+            lg={3}
             display={'flex'}
             justifyContent={'center'}
             alignItems={'center'}
             flexDirection={'column'}
         >
-            <Card sx={{ minWidth: 200 }}>
+            <Card sx={{ minWidth: 180 }}>
                 <CardContent>
                     <Stack
                         direction="row"
                         alignItems={'center'}
                         divider={<Divider orientation="vertical" flexItem />}
-                        spacing={2}
+                        spacing={0}
+                        justifyContent={'space-around'}
                     >
-                        <Box sx={{ position: 'relative', width: { xs: '30%' }, height: { xs: 40 } }}>
+                        <Box
+                            position={'relative'}
+                            width={{ xs: '30%' }}
+                            height={{ xs: 40 }}
+                        >
                             <Image
-                                src={image}
+                                className={'shadow-global'}
+                                src={`${image}`}
                                 alt={name}
                                 layout="fill"
                                 objectFit="fill"
                             />
                         </Box>
-                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                            {capitalize(name)}
+                        <Typography fontSize={14} fontWeight={900} color="text.secondary" gutterBottom>
+                            {capitalize(`${name}`)}
                         </Typography>
                     </Stack>
                 </CardContent>
