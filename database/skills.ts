b6e06abd9818165ec1687abbe1e9adb075ae321f
Jackson Quintero <codigoarchivo@gmail.com>
2022-12-03 17:54:40 -0400

    database/: creo database seed de servicios y categoria

---
+++ database/skills.ts
@@ -0,0 +1,69 @@
+import { ICategory, IEntry } from "../interfaces"
+
+
+export const servicios: IEntry[] = [
+    {
+        image: '/img/javascript.png',
+        name: 'javaScript',
+        type: 'lenguajes',
+        id: '96946597645987495874',
+        cat: '96946597645243525987495874'
+
+    },
+    {
+        image: '/img/typescript.png',
+        name: 'typeScript',
+        type: 'lenguajes',
+        id: '96946597645987495874434343',
+        cat: '96946597645243525987495874'
+
+    },
+    {
+        image: '/img/python.png',
+        name: 'python',
+        type: 'lenguajes',
+        id: '96946597645987495874434343',
+        cat: '96946597645243525987495874'
+
+    },
+    {
+        image: '/img/reactjs.png',
+        name: 'react',
+        type: 'librerías',
+        id: '96946597645987495874',
+        cat: '96946597645987sffefsf495874'
+    },
+    {
+        image: '/img/next.png',
+        name: 'Next.js',
+        type: 'framework',
+        id: '96946597645987495874',
+        cat: '969465976459ffsfs2352587495874'
+    },
+    {
+        image: '/img/git.png',
+        name: 'Git',
+        type: 'herramienta',
+        id: '96946597645987495874',
+        cat: '969465976455345345987495874'
+    }
+]
+
+export const categorias: ICategory[] = [
+    {
+        type: 'lenguajes',
+        id: '96946597645243525987495874',
+    },
+    {
+        type: 'librerías',
+        id: '96946597645987sffefsf495874',
+    },
+    {
+        type: 'framework',
+        id: '969465976459ffsfs2352587495874',
+    },
+    {
+        type: 'herramienta',
+        id: '969465976455345345987495874',
+    },
+]
