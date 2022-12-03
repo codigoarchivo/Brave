dac9019e96391c9f7a75c70177b4f7efcb2753d1
Jackson Quintero <codigoarchivo@gmail.com>
2022-12-03 17:34:26 -0400

    components/ui/Breadcrumb.tsx: estructure SomeButton

--- components/ui/Breadcrumb.tsx
+++ components/ui/Breadcrumb.tsx
@@ -1,24 +1,25 @@
-import React from 'react'
-
-import Breadcrumbs from '@mui/material/Breadcrumbs'
-
-import {  SomeButton } from './';
-
-import { RutaLink } from '../routes';
-
+import Breadcrumbs from '@mui/material/Breadcrumbs';
 import { useTheme } from '@mui/material';
+import { SomeButton } from './';
+import { RutaLink } from '../routes';
 
 export const Breadcrumb = () => {
   // rutaLink
   const data = RutaLink();
-
   const theme = useTheme();
-  
+
   return (
     <Breadcrumbs aria-label="breadcrumb" sx={{ padding: "5px 35px" }}>
       {
         data.map((item, key) =>
-          <SomeButton {...item} color={theme.palette.text.secondary} variant={'primary'} startIcon={<item.startIcon />} key={key} size={"small"} />
+          <SomeButton
+            {...item}
+            color={theme.palette.text.secondary}
+            variant={'primary'}
+            startIcon={<item.startIcon />}
+            key={key}
+            size={"small"}
+          />
         )
       }
     </Breadcrumbs>
