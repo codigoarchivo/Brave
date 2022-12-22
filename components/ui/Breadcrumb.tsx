5c8177e56fd94690af1c66bfcdde790a027a6be6
Jackson Quintero <codigoarchivo@gmail.com>
2022-12-22 19:00:56 -0400

    components/ui/Breadcrumb.tsx: cambio color del boton

--- components/ui/Breadcrumb.tsx
+++ components/ui/Breadcrumb.tsx
@@ -1,27 +1,23 @@
 import Breadcrumbs from '@mui/material/Breadcrumbs';
-import { useTheme } from '@mui/material';
 import { SomeButton } from './';
 import { RutaLink } from '../routes';
 
 export const Breadcrumb = () => {
   // rutaLink
   const data = RutaLink();
-  const theme = useTheme();
-
   return (
-    <Breadcrumbs aria-label="breadcrumb" sx={{ padding: "5px 35px" }}>
+    <>
       {
         data.map((item, key) =>
           <SomeButton
             {...item}
-            color={theme.palette.text.secondary}
-            variant={'primary'}
+            color={'secondary'}
             startIcon={<item.startIcon />}
             key={key}
-            size={"small"}
+            size={"large"}
           />
         )
       }
-    </Breadcrumbs>
+    </>
   )
 }
