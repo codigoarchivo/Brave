f29a8e2b48e5520146399d0e9daf565b7aa5a3ae
Jackson Quintero <codigoarchivo@gmail.com>
2022-12-22 19:05:19 -0400

    components/work/WorkScreen.tsx: se mejoro la estructura de la card

--- components/work/WorkScreen.tsx
+++ components/work/WorkScreen.tsx
@@ -1,21 +1,12 @@
 import { FC } from 'react';
-
 import Image from 'next/image';
-
 import Card from '@mui/material/Card';
-
 import CardContent from '@mui/material/CardContent';
-
 import Typography from '@mui/material/Typography';
-
 import CardActions from '@mui/material/CardActions';
-
 import Box from '@mui/material/Box';
-
 import Stack from '@mui/material/Stack';
-
 import Button from '@mui/material/Button';
-
 import { capitalize } from '@mui/material';
 
 interface Props {
@@ -23,36 +14,50 @@ interface Props {
     description: string;
     status: string;
     image: string;
+    idx: number;
 }
 
-export const WorkScreen: FC<Props> = ({ name, description, status, image }) => {
+export const WorkScreen: FC<Props> = ({ description, status, image, idx }) => {
     return (
-        <Card>
-            <CardContent >
-                <Box sx={{ margin: '0 auto', position: 'relative', width: { xs: '100%' }, height: { xs: 300 } }}>
-                    <Image
-                        src={image}
-                        alt={name}
-                        layout="fill"
-                        objectFit="fill"
-                    />
-                </Box>
-                <Stack sx={{ padding: '1rem' }}>
-                    <Typography variant='h2' color="text.secondary" gutterBottom>
-                        {capitalize(name)}
-                    </Typography>
-                    <Typography variant='body1' color="text.secondary" gutterBottom>
-                        {description}
-                    </Typography>
-                    <Typography variant='body2' color="text.secondary" gutterBottom>
-                        {status}
-                    </Typography>
-                </Stack>
-            </CardContent>
-            <CardActions sx={{ margin: '0 0 1rem 1rem' }}>
-                <Button size="small">Learn More</Button>
-            </CardActions>
-        </Card>
-
+        <>
+            <Card sx={{ height: { xs: 440 } }} >
+                <CardContent >
+                    <Box sx={{
+                        margin: '0 auto',
+                        position: 'relative',
+                        width: { xs: '100%' },
+                        height: { xs: 200 }
+                    }}
+                    >
+                        <Image
+                            src={image}
+                            alt={`Proyecto #${idx}`}
+                            layout="fill"
+                            objectFit="fill"
+                        />
+                    </Box>
+                    <Stack spacing={2}>
+                        <Typography variant='h5' color="text.secondary" gutterBottom>
+                            {capitalize(`Proyecto #${idx}`)}
+                        </Typography>
+                        <Typography variant='body1' color="text.secondary" gutterBottom>
+                            Tecnologías: <span style={{ fontWeight: 100 }}>{description}</span>
+                        </Typography>
+                        <Typography variant='body2' color="text.secondary" gutterBottom>
+                            Status: <span style={{ fontWeight: 100 }}>{status}</span>
+                        </Typography>
+                    </Stack>
+                </CardContent>
+                <CardActions sx={{
+                    justifyContent: 'flex-end',
+                    marginRight: '20px'
+                }}>
+                    <Button
+                        variant='outlined'
+                        size="small"
+                    >Learn More</Button>
+                </CardActions>
+            </Card>
+        </>
     )
 }
