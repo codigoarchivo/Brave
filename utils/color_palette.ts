d908ab394c3844dc693ff1205de0b02fd2e83c0b
Jackson Quintero <codigoarchivo@gmail.com>
2022-12-22 19:15:38 -0400

    utils/color_palette.ts: cambio de color en la variable

--- utils/color_palette.ts
+++ utils/color_palette.ts
@@ -2,4 +2,4 @@ export const COLOR_ONE = '#2f92b7';
 export const COLOR_TWO = '#fff';
 export const COLOR_THREE = '#69c2e9';
 export const COLOR_FOUR = '#69c2e9 2px 2px 20px 0px';
-export const COLOR_FIVE = '#00000099';
+export const COLOR_FIVE = '#121212';
