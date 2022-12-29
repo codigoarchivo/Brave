a4d7b75774da11e1d13708982dc03c37a31fb3ad
Jackson Quintero <codigoarchivo@gmail.com>
2022-12-29 16:44:48 -0400

    utils/color_palette.ts: agregue un shadow

--- utils/color_palette.ts
+++ utils/color_palette.ts
@@ -3,3 +3,4 @@ export const COLOR_TWO = '#fff';
 export const COLOR_THREE = '#69c2e9';
 export const COLOR_FOUR = '#69c2e9 2px 2px 20px 0px';
 export const COLOR_FIVE = '#121212';
+export const COLOR_SIX = '0px 11px 15px -7px rgb(0 0 0 / 20%), 0px 24px 38px 3px rgb(0 0 0 / 14%), 0px 9px 46px 8px rgb(0 0 0 / 12%)';
