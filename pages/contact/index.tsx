8dad897a82cd361eb86e4114ee0b0224ae7e7a8d
Jackson Quintero <codigoarchivo@gmail.com>
2022-12-29 16:46:12 -0400

    pages/contact/index.tsx: coloque un container

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,47 +1,51 @@
-import { NextPage } from 'next'
-
+import { NextPage } from 'next';
 import Container from '@mui/material/Container';
-
 import Grid from '@mui/material/Grid';
-
-import { Layout } from '../../components/layouts'
-
+import { Layout } from '../../components/layouts';
 import { InformationScreen, ContactScreen } from '../../components/contact';
 
 const ContactPage: NextPage = () => {
   return (
     <Layout title={"Contact"}>
-      <Grid
-        display={'flex'}
-        alignItems={'center'}
-        container
-        spacing={6}
-        sx={{ height: '100vh', backgroundColor: 'background.default' }}
-      >
+      <Container
+        sx={
+          {
+            height: '100vh',
+            display: 'flex',
+          }
+        }>
         <Grid
-          item
-          xs={12}
-          sm={12}
-          md={6}
           display={'flex'}
-          justifyContent={'center'}
           alignItems={'center'}
-          flexDirection={'column'} >
-          <InformationScreen />
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          sm={12}
-          md={6}
-          display={'flex'}
-          justifyContent={'center'}
-          alignItems={"center"}
-          sx={{ paddingBottom: { xs: 3, lg: 0 } }}
+          container
+          spacing={10}
+          mt={0}
         >
-          <ContactScreen />
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={6}
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            flexDirection={'column'} >
+            <InformationScreen />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={6}
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={"center"}
+            sx={{ paddingBottom: { xs: 3, lg: 0 } }}
+          >
+            <ContactScreen />
+          </Grid>
         </Grid>
-      </Grid>
+      </Container>
     </Layout>
   )
 }
